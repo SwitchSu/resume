b7d8b5bd375b50916ee069331ff726c9a875ebd0
SwitchSu <simon93107@gmail.com>
2024-05-22 23:00:10 +0800

    update-2

---
+++ js/all.js
@@ -0,0 +1,19 @@
+const pageTitle = document.querySelector('.page-title');
+const pages = document.querySelectorAll('.page');
+const abouts = document.querySelectorAll('.about ul li');
+abouts.forEach((about) => {
+  about.addEventListener('click', () => {
+    // console.log(pages[1]);
+    if (about.innerHTML == 'works') {
+      pageTitle.innerHTML = "My works";
+      pages[0].classList.add('close');
+      pages[1].classList.add('open');
+    } else if (about.innerHTML == 'contact') {
+      pageTitle.innerHTML = "Contact"
+    } else {
+      pageTitle.innerHTML = "Profile";
+      pages[0].classList.remove('close');
+      pages[1].classList.remove('open');
+    }
+  })
+})
